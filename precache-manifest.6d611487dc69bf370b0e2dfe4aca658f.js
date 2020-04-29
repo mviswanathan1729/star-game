de8a521fe948d4fbe5ce3f71665b9bf40e9f1359
Manohar Viswanathan <AB75535@LC02W109MHTDD>
2020-04-28 22:15:14 -0400

    Updates

---
+++ precache-manifest.6d611487dc69bf370b0e2dfe4aca658f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdb12c8972d3dfbf9b5e7c16101b37e",
+    "url": "/star-game/index.html"
+  },
+  {
+    "revision": "eaed16c02222f8ec02d0",
+    "url": "/star-game/static/css/main.cf2b60ff.chunk.css"
+  },
+  {
+    "revision": "fac93ee4886bc2d17932",
+    "url": "/star-game/static/js/2.1a30e953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-game/static/js/2.1a30e953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaed16c02222f8ec02d0",
+    "url": "/star-game/static/js/main.43f5556d.chunk.js"
+  },
+  {
+    "revision": "e9d73905950a3c432037",
+    "url": "/star-game/static/js/runtime-main.64a11a88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
